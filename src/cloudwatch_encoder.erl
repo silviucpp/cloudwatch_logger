1c3e9e76a8265cd48a8c255f39c81c11907442ee
silviu caragea <silviu.cpp@gmail.com>
2024-09-26 22:24:17 +0300

    Disable formatter for contain events

--- src/cloudwatch_encoder.erl
+++ src/cloudwatch_encoder.erl
@@ -4,21 +4,31 @@
     encode/3
 ]).
 
-encode(#{level := Level, meta := Meta} = Event, Hostname, {FormatterModule, FormatterConfig}) ->
+% no_formatter and hostname inside event are related to some internal apps.
+
+encode(#{level := Level, meta := Meta} = Event, Hostname, Formatter) ->
     {Module, _Function, _Arity} = maps:get(mfa, Meta, {null, null, null}),
     Ts = erlang:trunc(maps:get(time, Meta)/1000),
     {[
         {<<"timestamp">>, Ts},
         {<<"message">>, cloudwatch_utils:safe_json_encode([
             {<<"sv">>, severity2int(Level)},
-            {<<"host">>, maps:get(hostname, Meta, Hostname)},
+            {<<"host">>, maps:get(hostname, Event, Hostname)},
             {<<"tags">>, cloudwatch_utils:to_binary(Module)},
             {<<"ts">>, Ts},
-            {<<"msg">>, cloudwatch_utils:to_binary(FormatterModule:format(Event, FormatterConfig))}
+            {<<"msg">>, format_message(Event, Formatter)}
         ])}
     ]}.
 
 % internals
+
+format_message(Event, {FormatterModule, FormatterConfig}) ->
+    case maps:get(no_formatter, Event, false) of
+        true ->
+            maps:get(msg, Event);
+        _ ->
+            cloudwatch_utils:to_binary(FormatterModule:format(Event, FormatterConfig))
+    end.
 
 severity2int(debug) ->
     7;
